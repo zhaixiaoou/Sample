65e5dde9492ae1f42a2ee0e6a40d5e31afd95c5a
zhaixiaoou <zhaixiaoou>
2021-12-19 15:33:06 +0800

    feat: init、单例模式几种方式

---
+++ lib/src/main/java/com/zxo/lib/base/Singleton.java
@@ -0,0 +1,49 @@
+package com.zxo.lib.base;
+
+/**
+ * 单例模式的几种方式
+ * 1、懒汉式
+ * 2、饿汉式
+ * 3、双重校验锁
+ * 4、静态内部类
+ */
+public class Singleton {
+
+    // 1、饿汉式
+//    private static Singleton instance = new Singleton();
+//    private Singleton(){}
+//    public static Singleton newInstance(){
+//        return instance;
+//    }
+    // 2、懒汉式
+//    private static Singleton instance = null;
+//    private Singleton(){}
+//    public static Singleton newInstance(){
+//        if(null == instance){
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+    // 3、double-check 双重校验锁 + volatile
+//    private static volatile Singleton instance = null;
+//    private Singleton(){}
+//    public static Singleton getInstance() {
+//        if (instance == null) {
+//            synchronized (Singleton.class) {
+//                if (instance == null) {//2
+//                    instance = new Singleton();
+//                }
+//            }
+//        }
+//        return instance;
+//    }
+
+    // 4、静态内部类
+    private static class SingletonHolder{
+        public static Singleton instance = new Singleton();
+    }
+    private Singleton(){}
+    public static Singleton newInstance(){
+        return SingletonHolder.instance;
+    }
+}
